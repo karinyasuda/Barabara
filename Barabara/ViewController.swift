6b73dae6af148c989be075da1c36978255f4a72f
Karin <karin.03k35@gmail.com>
2016-02-07 14:03:06 +0900

    Initial Commit

---
+++ Barabara/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Barabara
+//
+//  Created by Karin on 2016/02/07.
+//  Copyright © 2016年 Karin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
